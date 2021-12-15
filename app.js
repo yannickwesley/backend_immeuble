a93bb7bc013c7f335854078353ddcd2c4866b08b
yannickwesley <yannickkarimou@gmail.com>
2021-12-15 21:11:33 +0000

    'connection'

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const appRoute = require("./routes/routetest")
+const appRoute = require("./routes/routetest");
+const userRoutes = require("./routes/user");
 mongoose.connect('mongodb+srv://wkdev:123Codez@cluster0.hqp5h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -16,5 +17,6 @@ app.use((req, res, next) => {
     next();
 });
 app.use('/api', appRoute);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
